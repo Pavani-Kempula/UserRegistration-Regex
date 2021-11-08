5d655b1cd66c0b13a0e80931cbb7706c2950aaa0
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-08 15:06:37 +0530

    ADDED : Checking if entered password is contains 8 characters or not

--- src/main/java/com/bridgelabz/userregistration/UserRegMain.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegMain.java
@@ -151,7 +151,6 @@ public class UserRegMain {
 
     public static void main(String[] args) {
         UserRegMain userObject = new UserRegMain();
-
         //calling methods.
         userObject.enterFirstName();
         userObject.enterLastName();
